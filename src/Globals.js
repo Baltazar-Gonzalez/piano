6f6e72aa4b59f06cb66532f0f119697ea503bda6
Baltazar Gonzalez <baltagonzalez1@gmail.com>
2021-09-11 16:39:28 -0300

    version test

--- src/Globals.js
+++ src/Globals.js
@@ -1,5 +1,4 @@
 const acordes2 = [
-<<<<<<< HEAD
     {key:"q", value: "DO4", type:"white"},
     {key:"2", value: "REb4", type:"black"},
     {key:"w", value: "RE4", type:"white"},
@@ -17,25 +16,6 @@ const acordes2 = [
     {key:"o", value: "RE5", type:"white"},
     {key:"0", value: "MIb5", type:"black"},
     {key:"p", value: "MI5", type:"white"},
-=======
-    {key: "q",value: "DO4",type:"white"},
-    {key:"2" ,value: "REb4",type:"black"},
-    {key:"w" ,value: "RE4",type:"white"},
-    {key:"3" ,value: "MIb4",type:"black"},
-    {key:"e" ,value: "MI4",type:"white"},
-    {key:"r" ,value: "FA4",type:"white"},
-    {key:"5" ,value: "SOLb4",type:"black"},
-    {key:"t" ,value: "SOL4",type:"white"},
-    {key:"6" ,value: "LAb4",type:"black"},
-    {key:"y" ,value: "LA4",type:"white"},
-    {key:"7" ,value: "SIb4",type:"black"},
-    {key:"u" ,value: "SI4",type:"white"},
-    {key:"i" ,value: "DO5",type:"white"},
-    {key:"9" ,value: "REb5",type:"black"},
-    {key:"o" ,value: "RE5",type:"white"},
-    {key:"0" ,value: "MIb5",type:"black"},
-    {key:"p" ,value: "MI5",type:"white"},
->>>>>>> 79003e3de6430da2945b5d7fd675c90620980d9c
   ]
 
   const acordes = {
